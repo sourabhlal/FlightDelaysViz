b1d780478d5cd6cdcb04aea96a07e532debee639
Romain Gehrig <romain.gehrig@gmail.com>
2017-12-09 00:21:46 +0100

    Create a class for the timeslider to improve modularity

--- timeslider.js
+++ timeslider.js
@@ -1,71 +1,75 @@
-// new Date(year, month, day, hours, minutes, seconds, milliseconds);
-let min_date = new Date(1999, 1, 1);
-let max_date = new Date(2017, 8, 1);
+class TimeSlider {
+    constructor(options) {
+        this.containerDiv = options.containerDiv;
+        this.startDate = options.startDate;
+        this.endDate = options.endDate;
+    }
 
-const chartDiv = document.getElementById("timeslider");
-const width = chartDiv.clientWidth * 0.95,
-      height = 50,
-      contextHeight = height,
-      contextWidth = width;
+    draw() {
+        const startDate = this.startDate;
+        const endDate = this.endDate;
+        const width = this.containerDiv.clientWidth;
+        const height = this.containerDiv.clientHeight;
+        const contextHeight = height;
+        const contextWidth = width;
 
-const svg = d3.select("#timeslider").append('svg')
-          .attr("width", (width))
-          .attr("height", (height));
+        const svg = d3.select(this.containerDiv).append('svg')
+                  .attr("width", (width))
+                  .attr("height", (height));
 
-svg.data([{}], createChart);
+        this.xScale = d3.scaleTime()
+                .range([0, width])
+                .domain([startDate, endDate]);
 
-function createChart(data) {
-  let startYear = min_date;
-  let endYear = max_date;
+        // Create a context for a brush
+        this.contextXScale = d3.scaleTime()
+                .range([0, contextWidth])
+                .domain(this.xScale.domain());
 
-  var xScale = d3.scaleTime()
-          .range([0, width])
-          .domain([min_date, max_date]);
+        this.contextAxis = d3.axisBottom(this.contextXScale)
+                .tickSize(contextHeight)
+                .tickPadding(-10);
 
-  // Create a context for a brush
-  var contextXScale = d3.scaleTime()
-    .range([0, contextWidth])
-    .domain(xScale.domain());
+        this.contextArea = d3.area()
+                .x(function(d) {
+                    return this.contextXScale(d.date);
+                })
+                .y0(contextHeight)
+                .y1(0)
+                .curve(d3.curveLinear);
 
-  var contextAxis = d3.axisBottom(contextXScale)
-    .tickSize(contextHeight)
-    .tickPadding(-10);
+        var brush = d3.brushX()
+                .extent([
+                    [this.contextXScale.range()[0], 0],
+                    [this.contextXScale.range()[1], contextHeight]
+                ])
+                .on("brush", this.onBrush(this));
 
-  var contextArea = d3.area()
-    .x(function(d) {
-      return contextXScale(d.date);
-    })
-    .y0(contextHeight)
-    .y1(0)
-    .curve(d3.curveLinear);
+        let context = svg.append("g")
+                .attr("class", "context")
+                .attr("transform", "translate(0,0)");
 
-  var brush = d3.brushX()
-    .extent([
-      [contextXScale.range()[0], 0],
-      [contextXScale.range()[1], contextHeight]
-    ])
-    .on("brush", onBrush);
+        context.append("g")
+            .attr("class", "x axis top")
+            .attr("transform", "translate(0,0)")
+            .call(this.contextAxis);
 
-  let context = svg.append("g")
-    .attr("class", "context")
-    .attr("transform", "translate(0,0)");
+        context.append("g")
+            .attr("class", "x brush")
+            .call(brush)
+            .selectAll("rect")
+            .attr("y", 0)
+            .attr("height", contextHeight);
 
-  context.append("g")
-    .attr("class", "x axis top")
-    .attr("transform", "translate(0,0)")
-    .call(contextAxis);
+        // Brush handler. Get time-range startDate a brush and pass it to the charts.
+        // Could do snaping with https://gist.github.com/mbostock/6232620
+    }
 
-  context.append("g")
-    .attr("class", "x brush")
-    .call(brush)
-    .selectAll("rect")
-    .attr("y", 0)
-    .attr("height", contextHeight);
-
-  // Brush handler. Get time-range from a brush and pass it to the charts.
-  // Could do snaping with https://gist.github.com/mbostock/6232620
-  function onBrush() {
-    var b = d3.event.selection === null ? contextXScale.domain() : d3.event.selection.map(contextXScale.invert);
-      console.log("From: " + b[0] + " to: " + b[1]);
-  }
+    onBrush(chart) {
+        return () => {
+            const that = chart;
+            var b = d3.event.selection === null ? that.contextXScale.domain() : d3.event.selection.map(that.contextXScale.invert);
+            console.log("From: " + b[0] + " to: " + b[1]);
+        };
+    }
 }
